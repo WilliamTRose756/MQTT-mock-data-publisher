f90959ffd92a5bd8800001db849686ce3f22e226
William Rose <willrose999@gmail.com>
2024-01-03 20:05:42 -0500

    Updated Functionality

--- iot_publisher.py
+++ iot_publisher.py
@@ -1,25 +1,56 @@
 import paho.mqtt.client as mqtt
 import time
 import random
+import json
+
+# def on_connect(client, userdata, flags, rc):
+#     print("Connected with result code " + str(rc))
+#     client.subscribe("test/topic")
+
+# client = mqtt.Client()
+# client.on_connect = on_connect
+
+# client.connect("localhost", 1883, 60) # Connect to your MQTT broker (replace "localhost" with the broker's address if different)
+
+# # Loop to simualate continuous data publishing
+# while True:
+#    distance_beacon_1 = random.uniform(1, 30)
+#    distance_beacon_2 = random.uniform(1, 30)
+#    distance_beacon_3 = random.uniform(1, 30)
+
+#    # Create a payload string with the distances
+#    payload = f"{distance_beacon_1}, {distance_beacon_2}, {distance_beacon_3}"
+#    client.publish("beacon/distances", payload)
+#    print(f"Published: {payload}")
+
+#    time.sleep(2) # Wait for 2 seconds before sending next data
+
+
+broker_address = "localhost"
+port = 1883
+topic = "iot/sensors/data"  
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe("test/topic")
+    print("Connected with result code "+str(rc))
 
-client = mqtt.Client()
+client = mqtt.Client("PythonPublisher")
 client.on_connect = on_connect
 
-client.connect("localhost", 1883, 60) # Connect to your MQTT broker (replace "localhost" with the broker's address if different)
+client.connect(broker_address, port, 60)
 
-# Loop to simualate continuous data publishing
 while True:
-   distance_beacon_1 = random.uniform(1, 30)
-   distance_beacon_2 = random.uniform(1, 30)
-   distance_beacon_3 = random.uniform(1, 30)
 
-   # Create a payload string with the distances
-   payload = f"{distance_beacon_1}, {distance_beacon_2}, {distance_beacon_3}"
-   client.publish("beacon/distances", payload)
-   print(f"Published: {payload}")
+    distance_beacon_1 = random.uniform(1, 30)
+    distance_beacon_2 = random.uniform(1, 30)
+    distance_beacon_3 = random.uniform(1, 30)
 
-   time.sleep(2) # Wait for 2 seconds before sending next data
+    # Create a JSON object
+    payload = json.dumps({
+        "beacon_1_distance": distance_beacon_1,
+        "beacon_2_distance": distance_beacon_2,
+        "beacon_3_distance": distance_beacon_3,
+    })
+
+    client.publish(topic, payload)
+    print(f"Message Sent: {payload}")
+    time.sleep(5)  # Publish every 5 seconds
